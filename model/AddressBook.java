7d84492230f5fb197bac635c927fb6554166c987
dev01997D <devnandan01997@gmail.com>
2020-10-08 01:42:42 +0530

    Added: Ability to ensure no duplicate contact is added into addessBook functionality added

---
+++ model/AddressBook.java
@@ -0,0 +1,57 @@
+package com.blz.addressbooksystem.model;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+	private ArrayList<Contact> addressBook = new ArrayList<Contact>();
+
+	public ArrayList<Contact> getAddressBook() {
+		return this.addressBook;
+	}
+
+	public void setAddressBook(ArrayList<Contact> addressBook) {
+		this.addressBook = addressBook;
+	}
+
+	public void addContactToAddressBook(Contact contact) {
+		addressBook.add(contact);
+	}
+
+	public Contact editContact(String firstName, String lastName) {
+		Contact contactSearch = null;
+		for (Contact contact : addressBook) {
+			if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+				contactSearch = contact;
+				break;
+			}
+		}
+
+		return contactSearch;
+	}
+
+	public boolean removeContact(String firstName, String lastName) {
+		boolean remove = false;
+		for (Contact contact : addressBook) {
+			if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+				addressBook.remove(contact);
+				remove = true;
+				break;
+			}
+		}
+		return remove;
+	}
+	
+	public void printContactDetails() {
+		if (addressBook.isEmpty()) {
+			System.out.println("No details has been added till Now. Please add some details first!");
+		} else {
+			for (Contact c : addressBook) {
+				System.out.println("First name: " + c.getFirstName() + " Last name: " + c.getLastName() + " Address: "
+						+ c.getAddress() + " City Name: " + c.getCityName() + " Zip Number: " + c.getZipNumber()
+						+ " Phone Number: " + c.getPhoneNumber() + " Email Id: " + c.getEmailId());
+			}
+		}
+	}
+
+}
---
+++ model/Contact.java
@@ -0,0 +1,82 @@
+package com.blz.addressbooksystem.model;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String cityName;
+	private int zipNumber;
+	private long phoneNumber;
+	private String emailId;
+
+	// Getters And Setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public int getZipNumber() {
+		return zipNumber;
+	}
+
+	public void setZipNumber(int zipNumber) {
+		this.zipNumber = zipNumber;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	// Parametrized Constructor
+	public Contact(String firstName, String lastName, String address, String cityName, int zipNumber, long phoneNumber, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.cityName = cityName;
+		this.zipNumber = zipNumber;
+		this.phoneNumber = phoneNumber;
+		this.emailId = emailId;
+	}
+
+	public Contact() {
+	}
+}
